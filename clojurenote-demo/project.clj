e1355e7d424403b5702761b7ea2c3ece2b513df8
Mike Roberts <mike.b.roberts@gmail.com>
2013-09-20 17:01:29 -0400

    Demo app to show authentication flow

---
+++ clojurenote-demo/project.clj
@@ -0,0 +1,16 @@
+(defproject clojurenote-demo "0.1.0"
+  :description "Demonstration app to show usage of clojurenote"
+  :url "https://github.com/mikebroberts/clojurenote"
+  :dependencies [
+  	[org.clojure/clojure "1.4.0"]
+    [compojure "1.1.5"]
+    [environ "0.4.0"]
+    [clojurenote "0.1.0"]
+  ]
+  :plugins [
+    [lein-ring "0.8.2"]
+    [lein-environ "0.4.0"]
+    ]
+  :ring {:handler clojurenote-demo.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
